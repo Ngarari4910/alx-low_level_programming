f9fdbd14ab53a3576e541f533baee55945fa4f34
Ngarari4910 <ngararidnw@gmail.com>
2023-02-08 11:14:50 -0800

    whatmyname

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,33 +1,17 @@
 
-<<<<<<< HEAD
-=======
-
-/*
- * File: 0-whatsmyname.c
- * Auth: David Ngarari 
- */
-
->>>>>>> 99c24c71331085bb8932bc6c960a6bcbb1b202dd
 #include <stdio.h>
-
+#include main.h
 /**
- * main - Prints the program name, followed by a new line.
- * @argc: The number of arguments supplied to the program.
- * @argv: An array of pointers to the arguments.
- *
- * Return: Always o.
- */
-<<<<<<< HEAD
-
-int main(int argc, char *argv[]) {
-    printf(%sn, argv[0]);
-    return 0;
-=======
-int main(int __attribute__((__unused__)) argc, char *argv[])
+* main - prints its name + \n
+* @argc: number of arguments typed
+* @argv: array pointing targuments
+* Return: 0
+*/
+int main(int argc, char*argv[])
 {
-.0-whatsmyname.c.swp printf(%sn, argv[0]);
+      argc = 0;
 
-.0-whatsmyname.c.swp return (0);
->>>>>>> 99c24c71331085bb8932bc6c960a6bcbb1b202dd
+      printf(%sn, argv[argc]);
+      return (0);
 }
 
